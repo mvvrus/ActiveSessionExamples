9c3d646046715a5e5659b1dc6a88efd9fb69ed23
mvvrus <vmurav@yandex.ru>
2024-07-17 03:45:43 +0300

    Add SampleApplication (initial version)

---
+++ EnumSapmleApplication/Pages/EnumAdapterSample.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SapmleApplication.Pages
+{
+    public class EnumAdapterSampleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
